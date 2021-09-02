259a2cf6227eca39f19a4fc9716f8e0ee22de2e0
martinjwkim <martinki@usc.edu>
2021-09-01 17:26:57 -0700

    fix errors

--- src/CompanyCard.js
+++ src/CompanyCard.js
@@ -1,26 +1,31 @@
-import React, {useState} from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
 import "./CompanyCard.css";
-import default_company_img from "./default_company_img.png"
+import default_company_img from "./default_company_img.png";
 
-// {handle, name, description, logo_url} 
+// {handle, name, description, logo_url}
 function CompanyCard({ company }) {
-  const { handle, name, description, logo_url } = company
+  const { handle, name, description, logo_url } = company;
   const history = useHistory();
 
   const handleClick = () => {
     history.push(`/companies/${handle}`);
-  }
+  };
 
-  function testFunc(evt){
+  function testFunc(evt) {
     evt.target.src = default_company_img;
   }
 
   return (
-    <section onClick={handleClick} className="card CompanyCard" >
+    <section onClick={handleClick} className="card CompanyCard">
       <div className="CompanyCard-NameImg">
         <h5 className="CompanyCard-Name">{name}</h5>
-        <img src={logo_url} onError = {testFunc} alt={name} className="card-img-top Company-Img" />
+        <img
+          src={logo_url}
+          onError={testFunc}
+          alt={name}
+          className="card-img-top Company-Img"
+        />
       </div>
       <p className="CompanyCard-Description">{description}</p>
     </section>
--- src/Profile.js
+++ src/Profile.js
@@ -1,74 +1,74 @@
-import React, { useState, useContext } from 'react';
-import { Redirect } from 'react-router-dom'
+import React, { useState, useContext } from "react";
+import { Redirect } from "react-router-dom";
 import UserContext from "./userContext";
 import JoblyApi from "./JoblyApi";
 import "./Profile.css";
 
 function Profile() {
   const { user, setUser } = useContext(UserContext);
-  const formUser = { ...user }
-  delete formUser.username
-  delete formUser.jobs
+  const formUser = { ...user };
+  delete formUser.username;
+  delete formUser.jobs;
   const [formData, setFormData] = useState(formUser);
-  const [alert, setAlert] = useState()
+  const [alert, setAlert] = useState();
 
-  if (!user) return <Redirect to="/login" />
+  if (!user) return <Redirect to="/login" />;
 
-  const handleSubmit = evt => {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
     const patchUser = async () => {
-      formData.photo_url = formData.photo_url || "http://somedefault.photo"
-      try{
-        const updatedUser = await JoblyApi.patchUser(user.username, formData)
-        setFormData(updatedUser)
-        setUser(updatedUser)
-        localStorage.setItem('user', JSON.stringify(updatedUser));
-        setAlert({color: "success", msg: "You have successfully updated your profile."})
-        setTimeout(()=>setAlert(null), 2000)
+      formData.photo_url = formData.photo_url || "http://somedefault.photo";
+      try {
+        const updatedUser = await JoblyApi.patchUser(user.username, formData);
+        setFormData(updatedUser);
+        setUser(updatedUser);
+        localStorage.setItem("user", JSON.stringify(updatedUser));
+        setAlert({
+          color: "success",
+          msg: "You have successfully updated your profile.",
+        });
+        setTimeout(() => setAlert(null), 2000);
       } catch {
-        setAlert({color: "danger", msg: "Invalid credentials."})
-        setTimeout(()=>setAlert(null), 2000)
+        setAlert({ color: "danger", msg: "Invalid credentials." });
+        setTimeout(() => setAlert(null), 2000);
       }
-    }
-    patchUser()
+    };
+    patchUser();
   };
 
-  const handleChange = evt => {
+  const handleChange = (evt) => {
     const { name, value } = evt.target;
-    setFormData(oldFormData => ({
+    setFormData((oldFormData) => ({
       ...oldFormData,
-      [name]: value
-    }))
+      [name]: value,
+    }));
   };
 
   const inputs = () => {
-    return Object.keys(formUser)
-      .map(key => (
-        <div key={key} className="form-group">
-          <label htmlFor={key} style={{ textTransform: "capitalize" }}>
-            {key.split("_").join(" ")}
-          </label>
-          <input
-            id={key}
-            name={key}
-            value={formData[key]}
-            onChange={handleChange}
-            className="form-control"
-          />
-        </div>
-      ))
-  }
+    return Object.keys(formUser).map((key) => (
+      <div key={key} className="form-group">
+        <label htmlFor={key} style={{ textTransform: "capitalize" }}>
+          {key.split("_").join(" ")}
+        </label>
+        <input
+          id={key}
+          name={key}
+          value={formData[key] || ""}
+          onChange={handleChange}
+          className="form-control"
+        />
+      </div>
+    ));
+  };
 
   const showAlert = (color, msg) => (
-    <div class={`alert alert-${color}`}>
-      {msg}
-    </div>
-  )
+    <div class={`alert alert-${color}`}>{msg}</div>
+  );
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit} className="Profile-Form">
-        <h2 style={{margin: "15px auto"}}>Profile for {user.username}</h2>
+        <h2 style={{ margin: "15px auto" }}>Profile for {user.username}</h2>
         {inputs()}
         <div className="form-group">
           <label htmlFor="password">Password</label>
@@ -82,10 +82,7 @@ function Profile() {
             placeholder="Enter password to save changes."
           />
         </div>
-        {alert
-        ? showAlert(alert.color,alert.msg)
-        : null
-        }
+        {alert ? showAlert(alert.color, alert.msg) : null}
         <button className="btn btn-primary">Save Changes</button>
       </form>
     </div>
@@ -93,4 +90,3 @@ function Profile() {
 }
 
 export default Profile;
-
